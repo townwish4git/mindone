c8a6d0e14ca02306ed9f5a218f0e5f0dd70a7f86
townwish4git <tanweixi1@huawei.com>
2024-01-26 19:15:08 +0800

    run sampling script

--- examples/stable_diffusion_xl/demo/run_sampling.sh
+++ examples/stable_diffusion_xl/demo/run_sampling.sh
@@ -1,26 +1,33 @@
-# 环境变量
+# ==================================
+# 1. 设置环境变量 [只需关注 DEVICE_ID]
+# ==================================
 export LD_PRELOAD=/usr/local/python3.7.5/lib/python3.7/site-packages/torch/lib/libgomp-d22c30c5.so.1:$LD_PRELOAD
 export MS_PYNATIVE_GE=1
-export DEVICE_ID=0
-TASK_NAME="txt2img"
+export DEVICE_ID=0  # 指定使用的NPU
 
 
-# 参数设置 [修改采样参数]
-SAMPLING_TASK="Sampling task name"
-CKPT_FILE="CKPT file to load"
+# ==================================
+# 2. 推理参数设置
+# ==================================
+SAMPLING_TASK_NAME="Sampling task name"
+CKPT_FILE="CKPT file to load, use comma(,) to separate multiple .ckpt files"
+CONFIG_FILE="configs/inference/sd_xl_base.yaml"
 PROMPTS="Prompts lists text file, one row = one prompt"
-SAMPLER="Sampler, e.g. EulerAncestralSampler"
+SAMPLER="EulerAncestralSampler"
+TASK_TYPE="txt2img"
 SAMPLE_STEP=20
 SEED=0
-NUM_REPEAT=4 # how many pictures to generate via one prompt
+NUM_REPEAT=4
 
 
-# 准备
+# ==================================
+# 3. 推理准备
+# ==================================
 sdxl_dir="$(dirname "$(dirname "$(readlink -f "$0")")")"
 cd $sdxl_dir
 
 ckpt_filename=$(echo "${CKPT_FILE}" | awk -F'/' '{print $NF}')
-demo_save_path=demo/$SAMPLING_TASK/${ckpt_filename}@${SAMPLER}@${SAMPLE_STEP}steps
+demo_save_path=demo/$SAMPLING_TASK_NAME/${ckpt_filename}@${SAMPLER}@${SAMPLE_STEP}steps
 test -d $demo_save_path && demo_save_path=${demo_save_path}@$(date +'%Y-%m-%d-%H:%M:%S')
 mkdir -p $demo_save_path
 
@@ -31,10 +38,12 @@ while IFS= read -r line; do
 done < "$PROMPTS"
 
 
-# 推理
+# ==================================
+# 4. 推理
+# ==================================
 python demo/sampling_without_streamlit.py \
---task $TASK_NAME \
---config configs/inference/sd_xl_base.yaml \
+--task $TASK_TYPE \
+--config $CONFIG_FILE \
 --weight $CKPT_FILE \
 --sampler $SAMPLER \
 --sample_step $SAMPLE_STEP \
@@ -44,7 +53,9 @@ python demo/sampling_without_streamlit.py \
 --save_path $demo_save_path
 
 
-# 图片拼接
+# ==================================
+# 5. 多图拼接和收尾工作
+# ==================================
 original_path="$demo_save_path/txt2img/SDXL-base-1.0"
 prompts_length=$(wc -l < $PROMPTS)
 concated_path="$demo_save_path/txt2img/concat"
