f87e83d3edb0ccb48e7474e0b8151c995401507b
townwish4git <tanweixi1@huawei.com>
2024-05-31 12:02:34 +0800

    feat(diffusers/pipeliness): ControlNets(no Blip)

--- mindone/diffusers/models/controlnet.py
+++ mindone/diffusers/models/controlnet.py
@@ -733,7 +733,7 @@ class ControlNetModel(ModelMixin, ConfigMixin):
                         f"which requires the keyword argument `time_ids` to be passed in `added_cond_kwargs`"
                     )
                 time_ids = added_cond_kwargs.get("time_ids")
-                time_embeds = self.add_time_proj(time_ids.flatten())
+                time_embeds = self.add_time_proj(time_ids.flatten()).to(time_ids.dtype)
                 time_embeds = time_embeds.reshape((text_embeds.shape[0], -1))
 
                 add_embeds = ops.concat([text_embeds, time_embeds], axis=-1)
