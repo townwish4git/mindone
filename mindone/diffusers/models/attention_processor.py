c3bb570340f24c93ff56d542ad081a9190cace22
townwish4git <townwish2023@outlook.com>
2024-10-24 15:57:07 +0800

    dev stash

--- mindone/diffusers/models/attention_processor.py
+++ mindone/diffusers/models/attention_processor.py
@@ -817,38 +817,52 @@ class JointAttnProcessor:
     ) -> ms.Tensor:
         residual = hidden_states
 
-        batch_size, channel, height, width = (None,) * 4
-        input_ndim = hidden_states.ndim
-        if input_ndim == 4:
-            batch_size, channel, height, width = hidden_states.shape
-            hidden_states = hidden_states.view(batch_size, channel, height * width).swapaxes(1, 2)
-        context_input_ndim = encoder_hidden_states.ndim
-        if context_input_ndim == 4:
-            batch_size, channel, height, width = encoder_hidden_states.shape
-            encoder_hidden_states = encoder_hidden_states.view(batch_size, channel, height * width).swapaxes(1, 2)
-
-        batch_size = encoder_hidden_states.shape[0]
+        batch_size = hidden_states.shape[0]
 
         # `sample` projections.
         query = attn.to_q(hidden_states)
         key = attn.to_k(hidden_states)
         value = attn.to_v(hidden_states)
 
+        inner_dim = key.shape[-1]
+        head_dim = inner_dim // attn.heads
+
+        query = query.view(batch_size, -1, attn.heads, head_dim).swapaxes(1, 2)
+        key = key.view(batch_size, -1, attn.heads, head_dim).swapaxes(1, 2)
+        value = value.view(batch_size, -1, attn.heads, head_dim).swapaxes(1, 2)
+
+        if attn.norm_q is not None:
+            query = attn.norm_q(query)
+        if attn.norm_k is not None:
+            key = attn.norm_k(key)
+
         # `context` projections.
-        encoder_hidden_states_query_proj = attn.add_q_proj(encoder_hidden_states)
-        encoder_hidden_states_key_proj = attn.add_k_proj(encoder_hidden_states)
-        encoder_hidden_states_value_proj = attn.add_v_proj(encoder_hidden_states)
+        if encoder_hidden_states is not None:
+            encoder_hidden_states_query_proj = attn.add_q_proj(encoder_hidden_states)
+            encoder_hidden_states_key_proj = attn.add_k_proj(encoder_hidden_states)
+            encoder_hidden_states_value_proj = attn.add_v_proj(encoder_hidden_states)
 
         # attention
-        query = ops.cat([query, encoder_hidden_states_query_proj], axis=1)
-        key = ops.cat([key, encoder_hidden_states_key_proj], axis=1)
-        value = ops.cat([value, encoder_hidden_states_value_proj], axis=1)
+        encoder_hidden_states_query_proj = encoder_hidden_states_query_proj.view(
+            batch_size, -1, attn.heads, head_dim
+        ).swapaxes(1, 2)
+        encoder_hidden_states_key_proj = encoder_hidden_states_key_proj.view(
+            batch_size, -1, attn.heads, head_dim
+        ).swapaxes(1, 2)
+        encoder_hidden_states_value_proj = encoder_hidden_states_value_proj.view(
+            batch_size, -1, attn.heads, head_dim
+        ).swapaxes(1, 2)
 
-        query = attn.head_to_batch_dim(query)
-        key = attn.head_to_batch_dim(key)
-        value = attn.head_to_batch_dim(value)
+        if attn.norm_added_q is not None:
+            encoder_hidden_states_query_proj = attn.norm_added_q(encoder_hidden_states_query_proj)
+        if attn.norm_added_k is not None:
+            encoder_hidden_states_key_proj = attn.norm_added_k(encoder_hidden_states_key_proj)
 
-        hidden_states = ops.operations.nn_ops.FlashAttentionScore(1, scale_value=attn.scale)(
+        query = ops.cat([query, encoder_hidden_states_query_proj], axis=2)
+        key = ops.cat([key, encoder_hidden_states_key_proj], axis=2)
+        value = ops.cat([value, encoder_hidden_states_value_proj], axis=2)
+
+        hidden_states = ops.operations.nn_ops.FlashAttentionScore(1, scale_value=attn.scale, input_layout="BNSD")(
             query.to(ms.float16), key.to(ms.float16), value.to(ms.float16), None, None, None, attention_mask
         )[3].to(query.dtype)
         hidden_states = attn.batch_to_head_dim(hidden_states)
